655439cdfd4175747612fa4a4efd8beb504cea8d
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-07 20:36:30 +0200

    fichier AppRouter pour déterminer les routes path etc

---
+++ src/AppRouter.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
