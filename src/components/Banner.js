aa00a45deb7b44a9d905231ace6449c81ce2d2c8
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-09 18:29:41 +0200

    Création du banner en js et scss

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,7 @@ import '../styles/Banner.scss';
 export default function Banner({ imageSrc, title }) {
   return (
     <div className="banner" style={{ backgroundImage: `url(${imageSrc})` }}>
+      <div className='banner-overlay'></div>
       <h1>{title}</h1>
     </div>
   );
