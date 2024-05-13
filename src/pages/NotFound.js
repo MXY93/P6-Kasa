aecd59b069257e79b64391d47e7b07d8d75bf1bd
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-13 02:51:34 +0200

    Création de la page d'erreur et ajout de style

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/NotFound.scss'
+
 export default function NotFound() {
-    return <h2>Page Not Found</h2>;
+    return (
+        <div className="error-page">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d'accueil</Link>
+        </div>
+    );
 }  
