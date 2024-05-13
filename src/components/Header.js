b2d8e6badd25f700eb741bf15ae381ce89414e4d
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-13 02:42:49 +0200

    Ajout de l'attribut onClick pour le lien Accueil du header pour la fonctionnalité de retour

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import { useAppContext } from '../AppContext';
 import kasa from "../assets/kasa-logo.png";
 
 export default function Header() {
+  const { setShowSlideshow } = useAppContext();
   return (
     <header>
       <img src={kasa} alt="logo Kasa" className="header__logo"/>
       <nav>
         <ul>
           <li>
-            <Link to="/">Accueil</Link>
+            <Link to="/" onClick={() => setShowSlideshow(false)}>Accueil</Link>
           </li>
           <li>
             <Link to="/about">A Propos</Link>
