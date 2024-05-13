f847a7f880ec9fdc3d0e4b6611b8de96c6138d97
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-13 03:08:23 +0200

    Ajout de la gestion d'affichage lors du click sur la card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,36 @@
+import React, { useState } from 'react';
 import Banner from '../components/Banner';
 import Card from '../components/Card';
 import data from '../data/logements.json';
+import Slideshow from '../components/Slideshow';
 import bannerImg from "../assets/banner-bg.png";
+import { useAppContext } from '../AppContext';
 
 export default function Home() {
+  const [selectedProperty, setSelectedProperty] = useState(null);
+  const {showSlideshow, setShowSlideshow} = useAppContext();
+
+  const handleCardClick = (property) => {
+    setSelectedProperty(property);
+    setShowSlideshow(true);
+  };
+
   return (
     <div>
       <Banner imageSrc={bannerImg} title="Chez vous, partout et ailleurs" />
-      <div className="cards-list">
+      {!showSlideshow && (
+        <div className="cards-list">
         {data.map((property) => (
           <Card
             key={property.id}
             imageSrc={property.cover}
             title={property.title}
+            onClick={() => handleCardClick(property)}
           />
         ))}
       </div>
+      )}
+      {showSlideshow && selectedProperty && <Slideshow property={selectedProperty} />}
     </div>
   );
 }
