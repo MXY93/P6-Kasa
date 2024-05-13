ec0bcc4b6cf5d5cdb2961b83671f825c1aec03b3
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-13 03:06:26 +0200

    Ajout d'imports dans le fichier Aboutjs

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,16 @@
+import Banner from '../components/Banner';
+import bannerImg from "../assets/about-banner.png";
+import Collapse from '../components/Collapse';
+
 export default function About() {
-    return <h2>About Us</h2>;
+    return (<div>
+        <Banner imageSrc={bannerImg} opacity="0.3"/>
+        <div className='collapse-container'>
+            <Collapse title="Fiabilité"/>
+            <Collapse title="Respect"/>
+            <Collapse title="Service"/>
+            <Collapse title="Sécurité"/> 
+        </div>
+    </div>
+    );
 }
