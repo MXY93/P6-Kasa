8ca17f04e3a041a64793fc67df79832bb18c91d3
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-13 02:38:55 +0200

    Modification du composant banner concernant la réutilisation de style notamment l'opacité

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,13 @@
 import '../styles/Banner.scss';
 
-export default function Banner({ imageSrc, title }) {
+export default function Banner({ imageSrc, title, opacity }) {
+  const overlayStyle = {
+    backgroundColor: `rgba(0, 0, 0, ${opacity || 0.6})`
+  };
+
   return (
     <div className="banner" style={{ backgroundImage: `url(${imageSrc})` }}>
-      <div className='banner-overlay'></div>
+      <div className='banner-overlay' style={overlayStyle}></div>
       <h1>{title}</h1>
     </div>
   );
