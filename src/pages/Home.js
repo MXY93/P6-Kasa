ecb129a78cfe14c70b9b79b4090bcfd71a24217e
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-18 20:55:58 +0200

    Ajout de la gestion de l'affichage des URL correspondant aux propriétés sélectionnées

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,29 @@
-import React, { useState } from 'react';
 import Banner from '../components/Banner';
 import Card from '../components/Card';
 import data from '../data/logements.json';
-import Slideshow from '../components/Slideshow';
 import bannerImg from "../assets/banner-bg.png";
-import { useAppContext } from '../AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  const [selectedProperty, setSelectedProperty] = useState(null);
-  const {showSlideshow, setShowSlideshow} = useAppContext();
+  const navigate = useNavigate();
 
-  const handleCardClick = (property) => {
-    setSelectedProperty(property);
-    setShowSlideshow(true);
+  const handleCardClick = (propertyId) => {
+    navigate(`/property/${propertyId}`);
   };
 
   return (
     <div>
       <Banner imageSrc={bannerImg} title="Chez vous, partout et ailleurs" />
-      {!showSlideshow && (
-        <div className="cards-list">
+      <div className="cards-list">
         {data.map((property) => (
           <Card
             key={property.id}
             imageSrc={property.cover}
             title={property.title}
-            onClick={() => handleCardClick(property)}
+            onClick={() => handleCardClick(property.id)}
           />
         ))}
       </div>
-      )}
-      {showSlideshow && selectedProperty && <Slideshow property={selectedProperty} />}
     </div>
   );
 }
