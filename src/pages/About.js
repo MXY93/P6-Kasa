2fa6a71eefffde416ab9ca674731c4e76148c8cd
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-18 20:45:44 +0200

    Ajout du contenu des différents collapses de la page à propos

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,17 @@ import bannerImg from "../assets/about-banner.png";
 import Collapse from '../components/Collapse';
 
 export default function About() {
-    return (<div>
+    const fiabilityContent = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+    const respectContent = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    const serviceContent = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    const securityContent = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    return (<div className='about-content'>
         <Banner imageSrc={bannerImg} opacity="0.3"/>
         <div className='collapse-container'>
-            <Collapse title="Fiabilité"/>
-            <Collapse title="Respect"/>
-            <Collapse title="Service"/>
-            <Collapse title="Sécurité"/> 
+            <Collapse title="Fiabilité" children={fiabilityContent}/>
+            <Collapse title="Respect" children={respectContent}/>
+            <Collapse title="Service" children={serviceContent}/>
+            <Collapse title="Sécurité" children={securityContent}/> 
         </div>
     </div>
     );
