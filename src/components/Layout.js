f570d50f5911773428e7b3881f69a96a3a6ec31d
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-18 19:56:49 +0200

    Modification d'affichage du style de page en fonction de sa localisation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,23 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { useLocation } from 'react-router-dom';
 
 export default function Layout({ children }) {
+  const location = useLocation();
+  let pageClass = "";
+
+  if (location.pathname === "/about") {
+    pageClass = "about-page";
+} else if (location.pathname === "/") {
+    pageClass = "home-page";
+} else if (location.pathname.startsWith("/property/")) {
+    pageClass = "home-page";
+} else {
+    pageClass = "error-page";
+}
+
   return (
-    <div>
+    <div className={`wrapper ${pageClass}`}>
       <Header />
       <main>{children}</main>
       <Footer />
